6252e9fa5b2614b9661fdf163b71bab6995693b2
Unnati Gandhi <gandhiunnati611@gmail.com>
2024-04-15 13:37:39 +0530

    Update Readme File

--- src/components/Categoryitems.js
+++ src/components/Categoryitems.js
@@ -14,6 +14,7 @@ const Categoryitems = ({ categorySelectedIndex }) => {
       });
       setopenIndex(categorySelectedIndex);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [categorySelectedIndex]);
 
   const toggleAccodion = (index) => {
